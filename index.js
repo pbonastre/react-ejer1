5016b4eb7393552be454b184f0f87d2236d64046
pbonastre <bonastrep@gmail.com>
2019-05-07 12:44:13 +0200

    add react metodology

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 //React element
-const element
-const Hello = <h2>Bienvenido a React </h2>
-render(Hello,document.getElementById('root'))
+//const element
+//const Hello = <h2 title='algo'>Bienvenido a React </h2>
+//console.log(Hello)
+//render(Hello,document.getElementById('root'))
+
+//React Component
+function Hello (){
+  return <h1> bienvenido a React </h1>
+}
+
+render(<Hello/>, document.getElementById('root'))
