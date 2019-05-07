b5623cf3e27b847d9e2dcf2259c0f86fe944c08f
pbonastre <bonastrep@gmail.com>
2019-05-07 12:32:08 +0200

    const y render

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+//React element
+const element
+const Hello = <h2>Bienvenido a React </h2>
+render(Hello,document.getElementById('root'))
